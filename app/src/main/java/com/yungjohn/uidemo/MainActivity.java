f6734623fd6973837b34964598caa7bff5076ef2
princejohnnie <61082393+princejohnnie@users.noreply.github.com>
2021-04-08 23:22:20 +0100

    LoginActivity first commit

--- app/src/main/java/com/yungjohn/uidemo/MainActivity.java
+++ app/src/main/java/com/yungjohn/uidemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.yungjohn.uidemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
